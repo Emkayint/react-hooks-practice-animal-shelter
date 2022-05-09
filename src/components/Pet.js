d59ee1541e4261e70d6c68e44f0edafb2ee88dc9
Martin <mkmartinoes@gmail.com>
2022-05-09 12:04:15 +0300

    Get Data and display

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,24 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 
-function Pet() {
+function Pet({name, gender, type, description, isAdopted, weight, age }) {
+  const [checkAdopted, setcheckAdopted] = useState(isAdopted)
+
+  function handleAdopted(){
+    setcheckAdopted(true)
+  }
+
   return (
     <div className="card" data-testid="pet">
       <div className="content">
         <span className="header">
-          {/*'♀' OR '♂' */}
-          PET NAME
+          {gender === "male" ? '♀' :  '♂'}
+          {name.toUpperCase()}
         </span>
         <div className="meta">
-          <span className="date">PET TYPE</span>
+          <span className="date">{type}</span>
         </div>
         <div className="description">
-          <p>Age: PET AGE</p>
-          <p>Weight: PET WEIGHT</p>
+          <p>Age: {age}</p>
+          <p>Weight: {weight}</p>
         </div>
       </div>
       <div className="extra content">
-        <button className="ui disabled button">Already adopted</button>
-        <button className="ui primary button">Adopt pet</button>
+        {checkAdopted ?  <button className="ui disabled button">Already adopted</button> : <button className="ui primary button" onClick={ handleAdopted }>Adopt pet</button>}
       </div>
     </div>
   );
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 import Pet from "./Pet";
 
+
 function PetBrowser() {
-  return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>;
+  const [petsFromDb, setPetsFromDb] = useState([])
+  useEffect(()=> {
+    fetch(`http://localhost:3001/pets`)
+    .then(res => res.json())
+    .then(res => setPetsFromDb( res) )
+  }, [])
+
+  const petsToDisplay = petsFromDb.map(pet => (
+    <Pet 
+      key = {pet.id}
+      name = {pet.name}
+      gender = {pet.gender}
+      type = {pet.type}
+      description = {pet.description}
+      isAdopted = {pet.isAdopted}
+      weight = {pet.weight}
+      age = {pet.age}
+    />
+  ))
+  
+
+  return <div className="ui cards">
+    {petsToDisplay}
+  </div>;
 }
 
 export default PetBrowser;
